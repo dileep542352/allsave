757acbb0a799c3dfac8f0845fdb529ba483e9391
songsunmc <118031927+dileep542352@users.noreply.github.com>
2025-02-09 10:14:25 +0530

    Update frontend.py

--- main/plugins/frontend.py
+++ main/plugins/frontend.py
@@ -19,9 +19,9 @@ logging.getLogger("telethon").setLevel(logging.INFO)
 
 message = "Send me the message link you want to start saving from, as a reply to this message."
           
-process=[]
-timer=[]
-user=[]
+process = []
+timer = []
+user = []
 
 @gagan.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
 async def clone(event):
@@ -31,62 +31,74 @@ async def clone(event):
         reply = await event.get_reply_message()
         if reply.text == message:
             return
-    lit=event.text
-    li=lit.split("\n")
+    lit = event.text
+    li = lit.split("\n")
     if len(li) > 10:
         await event.reply("max 10 links per message")
         return
+
     for li in li:
         try:
             link = get_link(li)
             if not link:
-                return
+                continue
         except TypeError:
-            return
+            continue
             
         edit = await event.reply("Processing!")
         if f'{int(event.sender_id)}' in user:
-            return await edit.edit("Please don't spam links, wait until ongoing process is done.")
-        user.append(f'{int(event.sender_id)}')
+            await edit.edit("Please don't spam links, wait until ongoing process is done.")
+            return
         
-        if "|" in li:
-            url = li
-            url_parts = url.split("|")
-            if len(url_parts) == 2:
-                file_name = url_parts[1]
-        if file_name is not None:
-            file_name = file_name.strip()                
         try:
+            user.append(f'{int(event.sender_id)}')
+            
+            if "|" in li:
+                url = li
+                url_parts = url.split("|")
+                if len(url_parts) == 2:
+                    file_name = url_parts[1].strip()
+                    
             if 't.me/' not in link:
                 await edit.edit("invalid link")
-                ind = user.index(f'{int(event.sender_id)}')
-                user.pop(int(ind))
-                return
+                continue
+                
             if 't.me/+' in link:
                 q = await join(userbot, link)
                 await edit.edit(q)
+                continue
+                
+            msg_id = 0
+            try:
+                msg_id = int(link.split("/")[-1])
+            except ValueError:
+                if '?single' in link:
+                    link_ = link.split("?single")[0]
+                    msg_id = int(link_.split("/")[-1])
+                else:
+                    msg_id = -1
+                    
+            if msg_id == -1:
+                await edit.edit("Invalid message ID")
+                continue
+                
+            try:
+                await get_msg(userbot, Bot, event.sender_id, edit.id, link, msg_id, file_name)
+            except ValueError as ve:
+                if "Peer id invalid" in str(ve):
+                    await edit.edit("Invalid channel/chat ID. Please check the link.")
+                    continue
+                raise
+                
+        except FloodWait as fw:
+            await edit.edit(f'Try again after {fw.value} seconds due to floodwait from telegram.')
+            
+        except Exception as e:
+            logging.error(f"Error: {str(e)}", exc_info=True)
+            await edit.edit(f"An error occurred: {str(e)}")
+            
+        finally:
+            if f'{int(event.sender_id)}' in user:
                 ind = user.index(f'{int(event.sender_id)}')
                 user.pop(int(ind))
-                return
-            if 't.me/' in link:
-                msg_id = 0
-                try:
-                    msg_id = int(link.split("/")[-1])
-                except ValueError:
-                    if '?single' in link:
-                        link_ = link.split("?single")[0]
-                        msg_id = int(link_.split("/")[-1])
-                    else:
-                        msg_id = -1
-                m = msg_id
-                await get_msg(userbot, Bot, event.sender_id, edit.id, link, m, file_name)
-        except FloodWait as fw:
-            await gagan.send_message(event.sender_id, f'Try again after {fw.value} seconds due to floodwait from telegram.')
-            await edit.delete()
-        except Exception as e:
-            logging.info(e)
-            await gagan.send_message(event.sender_id, f"An error occurred during cloning of `{link}`\n\n**Error:** {str(e)}")
-            await edit.delete()
-        ind = user.index(f'{int(event.sender_id)}')
-        user.pop(int(ind))
-        time.sleep(1)
+            time.sleep(1)
